6ac1edf151e0879dd840e80253e1e17e11917e2b
Pavel Che <alien.killer3@bk.ru>
2022-01-25 20:46:24 +0300

    added the window of search

--- js/main.js
+++ js/main.js
@@ -10,5 +10,14 @@ $(function(){
         $('.arrow-down').toggleClass('menu__list-item--sub-menu-active');
     })
     
+    $('.header__search').on('click', function() {
+        $('.header__search-overlay').addClass('active');
+    })
+
+    $('.header__search-overlay').on('click', function(e) {
+        if(!(e.target.className === 'header__search-input')) {
+            $('.header__search-overlay').removeClass('active');
+        }
+    })
     
 });
